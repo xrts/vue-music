7915c4e0149ee982d4f854638a71800a38526e1d
cj <286316299@qq.com>
2018-09-02 11:41:26 +0800

    music-list&6 over

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -1,6 +1,6 @@
-import {getLyric} from 'api/song'
-import {ERR_OK} from 'api/config'
-import {Base64} from 'js-base64'
+// import {getLyric} from 'api/song'
+// import {ERR_OK} from 'api/config'
+// import {Base64} from 'js-base64'
 
 export default class Song {
   constructor({id, mid, singer, name, album, duration, image, url}) {
@@ -14,25 +14,25 @@ export default class Song {
     this.url = url
   }
 
-  getLyric() {
-    if (this.lyric) {
-      return Promise.resolve(this.lyric)
-    }
-
-    return new Promise((resolve, reject) => {
-      getLyric(this.mid).then((res) => {
-        if (res.retcode === ERR_OK) {
-          this.lyric = Base64.decode(res.lyric)
-          resolve(this.lyric)
-        } else {
-          reject('no lyric')
-        }
-      })
-    })
-  }
+  // getLyric() {
+  //   if (this.lyric) {
+  //     return Promise.resolve(this.lyric)
+  //   }
+  //
+  //   return new Promise((resolve, reject) => {
+  //     getLyric(this.mid).then((res) => {
+  //       if (res.retcode === ERR_OK) {
+  //         this.lyric = Base64.decode(res.lyric)
+  //         resolve(this.lyric)
+  //       } else {
+  //         reject('no lyric')
+  //       }
+  //     })
+  //   })
+  // }
 }
 
-export function createSong(musicData) {
+export function createSong(musicData, songVkey) {
   return new Song({
     id: musicData.songid,
     mid: musicData.songmid,
@@ -41,7 +41,7 @@ export function createSong(musicData) {
     album: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    url: `http://dl.stream.qqmusic.qq.com/C100${musicData.songmid}.m4a?vkey=${songVkey}&fromtag=66`
   })
 }
 
@@ -55,4 +55,3 @@ function filterSinger(singer) {
   })
   return ret.join('/')
 }
-
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,20 +1,70 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">1</div>
+    <music-list :title="title" :songs="this.songs" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
 import {mapGetters} from 'vuex'
+import {getSingerDetail, getMusic} from 'api/singer'
+import {ERR_OK} from 'api/config'
+import {createSong} from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 
 export default {
+  data() {
+    return {
+      songs: []
+    }
+  },
   computed: {
     ...mapGetters([
       'singer'
-    ])
+    ]),
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      return this.singer.avatar
+    }
   },
   created() {
-    console.log(this.singer)
+    this._getDetail()
+  },
+  methods: {
+    _getDetail() {
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+        return
+      }
+      getSingerDetail(this.singer.id).then((res) => {
+        if (res.code === ERR_OK) {
+          this.songs = this._normalizeSongs(res.data.list)
+          console.log(this.songs)
+        }
+      })
+    },
+    _normalizeSongs(list) {
+      let ret = []
+      list.forEach((item) => {
+        let {musicData} = item
+        if (musicData.songid && musicData.albummid) {
+          getMusic(musicData.songmid).then((res) => {
+            if (res.code === ERR_OK) {
+              const svkey = res.data.items
+              const songVkey = svkey[0].vkey
+              const newSong = createSong(musicData, songVkey)
+              ret.push(newSong)
+            }
+          })
+        }
+      })
+      console.log(ret)
+      return ret
+    }
+  },
+  components: {
+    MusicList
   }
 }
 </script>
@@ -22,14 +72,6 @@ export default {
 <style lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
 
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    bottom: 0
-    right: 0
-    background: $color-background
   .slide-enter-active,.slide-leave-active
     transition: all .3s
   .slide-enter,slide-leave-to
